05fd1c997f018be4ca5596055a9d4b172f77ce4c
yugobosanski <152861205+yugo34@users.noreply.github.com>
2026-03-05 17:54:00 +0300

    Resolve timezone issues in date handling

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T14:53:46.181Z
-Commit: Fix broken links in documentation
+Timestamp: 2026-03-05T14:53:59.965Z
+Commit: Resolve timezone issues in date handling
