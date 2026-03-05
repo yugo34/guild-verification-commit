c4152a35fad3846fbc98a84a96e88cddc42a85ac
yugobosanski <152861205+yugo34@users.noreply.github.com>
2026-03-05 17:54:16 +0300

    Fix broken links in documentation

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T14:53:55.382Z
-Commit: Correct typo in configuration file
+Timestamp: 2026-03-05T14:54:16.382Z
+Commit: Fix broken links in documentation
