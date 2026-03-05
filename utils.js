03ae1b69c97402ab22b1434fac7a4386ed8fcfe5
yugobosanski <152861205+yugo34@users.noreply.github.com>
2026-03-05 20:29:53 +0300

    Fix CSS styling bug on mobile devices

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T17:26:50.002Z
-Commit: Implement user profile customization
+Timestamp: 2026-03-05T17:29:52.688Z
+Commit: Fix CSS styling bug on mobile devices
