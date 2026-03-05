1470b0fcba0078a6b67dc99f33e336c93f8ec7c0
yugobosanski <152861205+yugo34@users.noreply.github.com>
2026-03-05 17:48:09 +0300

    Resolve null pointer exception in user service

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T14:48:07.167Z
-Commit: Patch security vulnerability in authentication
+Timestamp: 2026-03-05T14:48:08.967Z
+Commit: Resolve null pointer exception in user service
