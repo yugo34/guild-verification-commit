7b07409d4455b88f98367cd45f4ed38960ac8ea1
yugobosanski <152861205+yugo34@users.noreply.github.com>
2026-03-05 19:49:26 +0300

    Patch API endpoint error handling

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T14:54:16.382Z
-Commit: Fix broken links in documentation
+Timestamp: 2026-03-05T16:49:26.220Z
+Commit: Patch API endpoint error handling
