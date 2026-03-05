f2545fefd71372b49854d811c4e26558841f400f
yugobosanski <152861205+yugo34@users.noreply.github.com>
2026-03-05 17:53:52 +0300

    Resolve timezone issues in date handling

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T14:53:49.098Z
-Commit: Correct typo in configuration file
+Timestamp: 2026-03-05T14:53:52.031Z
+Commit: Resolve timezone issues in date handling
