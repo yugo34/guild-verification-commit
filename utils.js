b33835e59f5d1416ea1421d375229ad7757626b7
yugobosanski <152861205+yugo34@users.noreply.github.com>
2026-03-05 17:54:14 +0300

    Fix memory leak in event handlers

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T14:53:59.965Z
-Commit: Resolve timezone issues in date handling
+Timestamp: 2026-03-05T14:54:13.648Z
+Commit: Fix memory leak in event handlers
