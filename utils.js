b0251dc946ade71e34a7547e4d14b85f27bf628c
yugobosanski <152861205+yugo34@users.noreply.github.com>
2026-03-05 20:17:26 +0300

    Introduce real-time notifications

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T17:16:27.559Z
-Commit: Fix CSS styling bug on mobile devices
+Timestamp: 2026-03-05T17:17:25.509Z
+Commit: Introduce real-time notifications
