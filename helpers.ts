e8b54fe92110400ab21de4f36718809c3debe790
yugobosanski <152861205+yugo34@users.noreply.github.com>
2026-03-05 20:27:18 +0300

    Implement user profile customization

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T17:17:13.094Z
-Commit: Fix broken links in documentation
+Timestamp: 2026-03-05T17:27:18.446Z
+Commit: Implement user profile customization
