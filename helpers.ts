021f02a280b67ba0d04bfefa22b3bfd6f6f5eace
yugobosanski <152861205+yugo34@users.noreply.github.com>
2026-03-05 20:17:13 +0300

    Fix broken links in documentation

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T17:12:57.864Z
-Commit: Implement user profile customization
+Timestamp: 2026-03-05T17:17:13.094Z
+Commit: Fix broken links in documentation
